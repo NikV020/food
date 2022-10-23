a24d11b2dd86211fb5186dbfea8ddd244051fbe6
Nikita <nikitka_ugatu@mail.ru>
2022-10-23 22:13:36 +0300

    import export modules

--- js/modules/cards.js
+++ js/modules/cards.js
@@ -1,3 +1,6 @@
+import {getResource} from '../services/services';
+
+
 function cards () {
     // Используем классы для создание карточек меню
 
@@ -42,15 +45,7 @@ function cards () {
         }
     }
 
-    const getResource = async (url) => {
-        const res = await fetch(url); 
 
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
-
-        return await res.json();
-    };
     
     getResource('http://localhost:3000/menu')
     .then(data => {
@@ -62,4 +57,4 @@ function cards () {
 
 }
 
-module.exports = cards;
+export default cards;
--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import slider from './modules/slider';
+import forms from './modules/forms';
+import calc from './modules/calc';
+import {openModal} from './modules/modal';
+
+
 window.addEventListener('DOMContentLoaded', () => {
- const tabs = require('./modules/tabs'),
-       modal = require('./modules/modal'),
-       timer = require('./modules/timer'),
-       cards = require('./modules/cards'),
-       slider = require('./modules/slider'),
-       forms = require('./modules/forms'),
-       calc = require('./modules/calc');
-    
-    tabs();
-    modal();
-    timer();
+
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2022-12-23');
     cards();
-    slider();
-    forms();
+    slider({
+        container:'.offer__slider',
+        nextArrow:'.offer__slider-next',
+        preArrow:'.offer__slider-prev',
+        slide:'.offer__slide',
+        totalCounter:'#total',
+        currentCounter:'#current',
+        wrapper:'.offer__slider-wrapper',
+        field:'.offer__slider-inner'
+    });
+    forms('form', modalTimerId);
     calc();
 
 });
